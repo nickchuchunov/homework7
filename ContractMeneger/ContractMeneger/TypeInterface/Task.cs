788b66d8936796850dc83f8736d18b04c9bcbacc
nickchuchunov <nick.chuchunov@yandex.u>
2021-08-15 14:49:42 +0300

    выполнение практического задания 2

---
+++ ContractMeneger/ContractMeneger/TypeInterface/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContractMeneger
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public Task() { }
+
+
+
+
+    }
+}
